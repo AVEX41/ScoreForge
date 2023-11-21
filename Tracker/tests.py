3ae233e488928e63c180c60eebc3418d2aa53183
AVEX41 <aleksander.hoff06@gmail.com>
2023-11-21 07:48:17 +0100

    made one test

--- Tracker/tests.py
+++ Tracker/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import User
 
 # Create your tests here.
+
+
+class IndexTests(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username="user1", password="password1")
+
+    def test_first(self):
+        self.client.login(username="user1", password="password1")
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
