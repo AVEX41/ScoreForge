f1a5c87565dc1c0362e0bdfc8d769a6e7bc66901
AVEX41 <aleksander.hoff06@gmail.com>
2023-11-30 17:58:29 +0100

    added to the gitignore and generate data

--- generate-data/generate-data.py
+++ generate-data/generate-data.py
@@ -16,12 +16,27 @@ def generate_data(num_rows):
 
 
 # Set the number of rows
-num_rows = 20  # You can change this to your desired number of rows
+num_rows = 30  # You can change this to your desired number of rows
 
 # Generate the DataFrame
 df = generate_data(num_rows)
 
-# Save to Excel file
-df.to_excel("output_file.xlsx", index=False)
+# Save to Excel file with formatting as a table
+output_file = "output_60_1.xlsx"
+with pd.ExcelWriter(output_file, engine="xlsxwriter") as writer:
+    df.to_excel(writer, sheet_name="Sheet1", index=False, header=True)
 
-print(f"Excel file 'output_file.xlsx' generated with {num_rows} rows.")
+    # Get the xlsxwriter workbook and worksheet objects
+    workbook = writer.book
+    worksheet = writer.sheets["Sheet1"]
+
+    # Get the dimensions of the DataFrame
+    num_rows, num_cols = df.shape
+
+    # Create a list of column headers to use in add_table()
+    column_settings = [{"header": column} for column in df.columns]
+
+    # Add the Excel table structure. Pandas will add the data automatically
+    worksheet.add_table(0, 0, num_rows, num_cols - 1, {"columns": column_settings})
+
+print(f"Excel file '{output_file}' generated with {num_rows} rows.")
