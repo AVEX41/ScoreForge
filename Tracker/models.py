05dbb12ddadd61f5c1e8e47f61beeacbf09cce2c
AVEX41 <aleksander.hoff06@gmail.com>
2023-11-30 12:04:29 +0100

    added to models, and made migrations

--- Tracker/models.py
+++ Tracker/models.py
@@ -13,16 +13,17 @@ class User(AbstractUser):
 class ScoreTable(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="score_tables"
-    )
-    timestamp = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
-    user_favourite = models.BooleanField()
+    )  # adding the user
+    timestamp = models.DateTimeField(auto_now_add=True)  # when
+    name = models.CharField(max_length=100)  # name of the table
+    description = models.CharField(max_length=500)  # description of the table
+    user_favourite = models.BooleanField()  # is it a user favourite
+    shots_count = models.IntegerField(null=True)  # how many shots/nodes per set
 
     def __str__(self):
         return f"{self.name} - {self.user.username}"
 
-    def serialize_score_sets(self):
+    def serialize_score_sets(self):  # serializer
         score_sets = self.score_sets.all()
         serialized_score_sets = serialize("json", score_sets)
         return json.loads(serialized_score_sets)
@@ -31,11 +32,11 @@ class ScoreTable(models.Model):
 class ScoreSet(models.Model):
     score_table = models.ForeignKey(
         ScoreTable, on_delete=models.CASCADE, related_name="score_sets"
-    )
-    timestamp = models.DateTimeField(auto_now_add=True)
-    int_score = models.IntegerField()
-    decimal_score = models.FloatField()
-    total_inners = models.IntegerField()
+    )  # linking to the mother/table
+    timestamp = models.DateTimeField(auto_now_add=True)  # when
+    int_score = models.IntegerField()  # the total score of the set
+    decimal_score = models.FloatField()  # total score of the set in decimal
+    total_inners = models.IntegerField()  # total inners
 
     def __str__(self):
         return f"ScoreSet - {self.id} - {self.timestamp}"
@@ -44,9 +45,9 @@ class ScoreSet(models.Model):
 class ScoreNode(models.Model):
     score_set = models.ForeignKey(
         ScoreSet, on_delete=models.CASCADE, related_name="score_nodes"
-    )
-    score = models.FloatField()
-    inner = models.BooleanField()
+    )  # linking to the mother/table
+    score = models.FloatField()  # the score of the shot
+    inner = models.BooleanField()  # was it an inner
 
     def __str__(self):
         return f"ScoreNode - {self.id} - Score: {self.score}, Inner: {self.inner}"
