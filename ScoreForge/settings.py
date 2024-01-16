3a8c50817282e4e9ebdd65abe84f0d35b6689138
AVEX41 <aleksander.hoff06@gmail.com>
2024-01-16 08:00:58 +0100

    added graph to the comp view

--- ScoreForge/settings.py
+++ ScoreForge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-4tboh3%p0$*baqzxa5(-dchmdr=#%psky=x#c&ya0@=krxvg0h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
--- Tracker/static/Tracker/comp-view.js
+++ Tracker/static/Tracker/comp-view.js
@@ -17,10 +17,10 @@ function comp_view(row) {
             const data = await response.json();
 
             // add data to page
-            
             document.getElementById("comp-view-title").innerHTML = data.performance_indicator_name;
             document.getElementById("comp-view-description").innerHTML = data.performance_indicator_description;
 
+            // ------------------- Add data to table -------------------
             // Create table
             let table = document.getElementById("comp-view-table-content");
             let headers = ["score", "timestamp"];
@@ -39,6 +39,7 @@ function comp_view(row) {
                 });
             });
 
+            // ------------------- Add event listener to new-button -------------------
             // remove old event listener if it exists
             document.getElementById("comp-view-new-btn").removeEventListener("click", () => {
                 //handle click
@@ -50,8 +51,61 @@ function comp_view(row) {
                 //handle click
                 comp_new(data)
             });
-            console.log("Added event listener to new-button: " + data);
-            console.log(data);
+
+            // ------------------- Add event listener to edit-button -------------------
+            // TODO
+
+            // ------------------- Add event listener to delete-button -------------------
+            // TODO
+
+            // ------------------- Add data to graph -------------------
+    
+            const data_points = data.data_points.map((data_point, index) => ({
+            x: index + 1, // Number each starting from one
+            y: data_point.fields.score, // Vertical value as int_score
+            }));
+    
+            const chartData = {
+            labels: data_points.map(scoreSet => scoreSet.x),
+            datasets: [
+                {
+                label: "Sample Data",
+                data: data_points,
+                borderColor: "rgba(75, 192, 192, 1)",
+                fill: false,
+                },
+            ],
+            };
+    
+            const config = {
+            type: "line",
+            data: chartData,
+            options: {
+                scales: {
+                x: {
+                    type: "linear", // Use linear scale for numeric x-axis
+                    position: 'bottom',
+                    title: {
+                    display: true,
+                    text: 'NBR',
+                    },
+                },
+                y: {
+                    beginAtZero: true,
+                    title: {
+                    display: true,
+                    text: 'score',
+                    },
+                },
+                },
+            },
+            };
+    
+            // Get the canvas element
+            const ctx = document.getElementById("chartjs-comp-view").getContext("2d");
+    
+            // Create the chart
+            const myChart = new Chart(ctx, config);
 
         } catch (error) {
             console.error('Error fetching data:', error);
