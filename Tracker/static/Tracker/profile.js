ea5f58aa7be9706d3b742c9d915b8e15e04a4579
AVEX41 <aleksander.hoff06@gmail.com>
2024-01-29 20:21:03 +0100

    Added to UI, not JS

--- Tracker/static/Tracker/profile.js
+++ Tracker/static/Tracker/profile.js
@@ -1,3 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    Array.from(document.getElementsByClassName("edit-form")).forEach(element => {
+        element.style.display = "none";
+    });
+});
+
 function profile_show_main(field) {
     console.log(field);
 }
