b1f030302602325d95c4a8b42acc4e25141bc127
AVEX41 <aleksander.hoff06@gmail.com>
2024-01-30 13:51:45 +0100

    updated profile to make the design better

--- Tracker/static/Tracker/profile.js
+++ Tracker/static/Tracker/profile.js
@@ -1,172 +1,102 @@
 document.addEventListener("DOMContentLoaded", () => {
-    Array.from(document.getElementsByClassName("edit-form")).forEach(element => {
-        element.style.display = "none";
-    });
+    const hideEditForms = () => {
+        document.querySelectorAll(".edit-form").forEach(element => {
+            element.style.display = "none";
+        });
+        document.querySelectorAll(".profile-main").forEach(element => {
+            element.style.display = "block";
+        });
+    };
+
+    const showEditForms = () => {
+        document.querySelectorAll(".edit-form").forEach(element => {
+            element.style.display = "block";
+        });
+        document.querySelectorAll(".profile-main").forEach(element => {
+            element.style.display = "none";
+        });
+
+        // add data to input
+    };
+
+    const showMainProfile = () => {
+        hideEditForms();
+    };
+
+    const handleFormSubmission = (form, url, successCallback) => {
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const formData = new FormData(form);
+
+            fetch(url, {
+                method: "POST",
+                body: formData,
+                headers: {
+                    "X-CSRFToken": formData.get("csrfmiddlewaretoken")
+                    // Add any other headers if needed
+                }
+            })
+            .then(response => {
+                if (response.ok) {
+                    successCallback(formData);
+                } else {
+                    console.error("Form submission failed with status: " + response.status);
+                }
+            })
+            .catch(error => {
+                console.error("Network error occurred while submitting the form:", error);
+            });
+        });
+    };
+
+    const handleNameFormSubmission = (formData) => {
+        console.log("name form submitted successfully");
+
+        document.getElementById("profile-first-name").innerHTML = formData.get("first_name");
+        document.getElementById("profile-last-name").innerHTML = formData.get("last_name");
+
+        showMainProfile();
+    };
+
+    const handleEmailFormSubmission = (formData) => {
+        console.log("email form submitted successfully");
+
+        document.getElementById("profile-email").innerHTML = formData.get("email");
+
+        showMainProfile();
+    };
+
+    const handleUserNameFormSubmission = (formData) => {
+        console.log("username form submitted successfully");
+
+        document.getElementById("profile-user-name").innerHTML = formData.get("user_name");
+
+        showMainProfile();
+    };
+
+    const handlePasswordFormSubmission = () => {
+        console.log("pword form submitted successfully");
+
+        showMainProfile();
+    };
+
+    // Initial setup
+    hideEditForms();
 
     document.getElementById("profile-show-edit").addEventListener("click", () => {
-        profile_show_form()
-    });
-    document.getElementById("profile-show-main").addEventListener("click", () => {
-        profile_show_main()
+        profile_show_form();
     });
 
+    document.getElementById("profile-show-main").addEventListener("click", showMainProfile);
 
-    // ---- Handle forms ----
-    name_form = document.getElementById("profile-name-edit-form");
-    email_form = document.getElementById("profile-email-edit-form");
-    user_name_form = document.getElementById("profile-user-name-edit-form");
-    pword_form = document.getElementById("profile-password-edit-form");
-
-    // Handle name
-    name_form.onsubmit = (event) => {
-        event.preventDefault();
-
-        var formData = new FormData(name_form);
-        
-        // Use fetch to send the form data
-        fetch("form/edit/usr/name", {
-            method: "POST",
-            body: formData,
-            headers: {
-                "X-CSRFToken": formData.get("csrfmiddlewaretoken")
-                // Add any other headers if needed
-            }
-        })
-        .then(response => {
-            // Check if the request was successful (status 2xx)
-            if (response.ok) {
-                // Handle the successful response here
-                console.log("name form submitted successfully");
-
-                document.getElementById("profile-first-name").innerHTML = formData.get("first_name");
-                document.getElementById("profile-last-name").innerHTML = formData.get("last_name");
-                
-                profile_show_main();
-            } else {
-                // Handle the error response here
-                console.error("Form submission failed with status: " + response.status);
-            }
-        })
-        .catch(error => {
-            // Handle network errors here
-            console.error("Network error occurred while submitting the form:", error);
-        });
-    }
-
-    // Handle email
-    email_form.onsubmit = (event) => {
-        event.preventDefault();
-
-        var formData = new FormData(email_form);
-        
-        // Use fetch to send the form data
-        fetch("form/edit/usr/email", {
-            method: "POST",
-            body: formData,
-            headers: {
-                "X-CSRFToken": formData.get("csrfmiddlewaretoken")
-                // Add any other headers if needed
-            }
-        })
-        .then(response => {
-            // Check if the request was successful (status 2xx)
-            if (response.ok) {
-                // Handle the successful response here
-                console.log("email form submitted successfully");
-
-                document.getElementById("profile-email").innerHTML = formData.get("email");
-                
-                profile_show_main();
-            } else {
-                // Handle the error response here
-                console.error("Form submission failed with status: " + response.status);
-            }
-        })
-        .catch(error => {
-            // Handle network errors here
-            console.error("Network error occurred while submitting the form:", error);
-        });
-    }
-
-    // Handle user name
-    user_name_form.onsubmit = (event) => {
-        event.preventDefault();
-
-        var formData = new FormData(user_name_form);
-        
-        // Use fetch to send the form data
-        fetch("form/edit/usr/usrname", {
-            method: "POST",
-            body: formData,
-            headers: {
-                "X-CSRFToken": formData.get("csrfmiddlewaretoken")
-                // Add any other headers if needed
-            }
-        })
-        .then(response => {
-            // Check if the request was successful (status 2xx)
-            if (response.ok) {
-                // Handle the successful response here
-                console.log("username form submitted successfully");
-                
-                document.getElementById("profile-user-name").innerHTML = formData.get("user_name");
-                
-                profile_show_main();
-            } else {
-                // Handle the error response here
-                console.error("Form submission failed with status: " + response.status);
-            }
-        })
-        .catch(error => {
-            // Handle network errors here
-            console.error("Network error occurred while submitting the form:", error);
-        });
-    }
-
-    // Handle pword
-    pword_form.onsubmit = (event) => {
-        event.preventDefault();
-
-        var formData = new FormData(pword_form);
-        
-        // Use fetch to send the form data
-        fetch("form/edit/usr/pword", {
-            method: "POST",
-            body: formData,
-            headers: {
-                "X-CSRFToken": formData.get("csrfmiddlewaretoken")
-                // Add any other headers if needed
-            }
-        })
-        .then(response => {
-            // Check if the request was successful (status 2xx)
-            if (response.ok) {
-                // Handle the successful response here
-                console.log("pword form submitted successfully");
-
-                profile_show_main();
-            } else {
-                // Handle the error response here
-                console.error("Form submission failed with status: " + response.status);
-            }
-        })
-        .catch(error => {
-            // Handle network errors here
-            console.error("Network error occurred while submitting the form:", error);
-        });
-    }
+    // Handle forms
+    handleFormSubmission(document.getElementById("profile-name-edit-form"), "form/edit/usr/name", handleNameFormSubmission);
+    handleFormSubmission(document.getElementById("profile-email-edit-form"), "form/edit/usr/email", handleEmailFormSubmission);
+    handleFormSubmission(document.getElementById("profile-user-name-edit-form"), "form/edit/usr/usrname", handleUserNameFormSubmission);
+    handleFormSubmission(document.getElementById("profile-password-edit-form"), "form/edit/usr/pword", handlePasswordFormSubmission);
 
 });
-
-function profile_show_main() {
-    // show correct bunch
-    Array.from(document.getElementsByClassName("edit-form")).forEach(element => {
-        element.style.display = "none";
-    });
-    Array.from(document.getElementsByClassName("profile-main")).forEach(element => {
-        element.style.display = "block";
-    });
-}
 
 function profile_show_form() {
     // show correct bunch
